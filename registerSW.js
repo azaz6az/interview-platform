e09a7b7f84c61dda6ad1d884981f4b550d8f5bd7
azaz6az <azaz6az@users.noreply.github.com>
2026-06-18 17:01:28 +0800

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/interview-platform/sw.js', { scope: '/interview-platform/' })})}
